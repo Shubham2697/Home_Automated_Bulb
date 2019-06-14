5a000c218c3ae09ef3bb132f527b8dfc0d810138
Shubham Agarwal <30969341+Shubham2697@users.noreply.github.com>
2019-06-14 14:29:00 +0530

    Add files via upload

---
+++ Home Automation-Bulb.ino
@@ -0,0 +1,46 @@
+#define BLYNK_PRINT SwSerial
+
+
+#include <SoftwareSerial.h>
+SoftwareSerial SwSerial(10, 11); // RX, TX
+    
+#include <BlynkSimpleStream.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "<your blynk auth ID>";
+
+WidgetLED led1(13);
+
+BlynkTimer timer;
+
+// V1 LED Widget is blinking
+void blinkLedWidget()
+{
+  if (led1.getValue()==0) {
+    led1.off();
+    SwSerial.println("LED on 13: off");
+  } else {
+    led1.on();
+    SwSerial.println("LED on 13: on");
+  }
+}
+
+void setup()
+{
+  // Debug console
+  SwSerial.begin(9600);
+
+  // Blynk will work through Serial
+  // Do not read or write this serial manually in your sketch
+  Serial.begin(9600);
+  Blynk.begin(Serial, auth);
+
+  timer.setInterval(1000L, blinkLedWidget);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
